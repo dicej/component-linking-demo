22d1405e5c3ef85573214c5c0cae68c8a966613b
Joel Dice <joel.dice@fermyon.com>
2023-06-07 17:51:22 -0600

    leave __main_void.o out of libc.so
    Signed-off-by: Joel Dice <joel.dice@fermyon.com>

--- bar_impl.c
+++ bar_impl.c
@@ -3,9 +3,3 @@
 int32_t exports_test_test_test_bar(int32_t arg) {
     return arg + 73;
 }
-
-
-__attribute__((__export_name__("__main_argc_argv")))
-int main_argc_argv(int argc, int argv) {
-    return 0;
-}
